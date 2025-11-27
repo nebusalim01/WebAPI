59cfcda92e88fc90d97dc5b686e27390bbf8fb05
Nebu Salim <siva01619@gmail.com>
2025-11-27 17:18:21 +0530

    Add project files.

---
+++ WebAPI/API/APIController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+
+namespace WebAPI.API
+{
+    public class APIController : ApiController
+    {
+        WebAPIEntities objdb = new WebAPIEntities();
+        // GET: api/API
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/API/5
+        [HttpGet]
+        [Route("api/API/getid_studenttable/{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            student_table student = objdb.student_table.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+        // POST: api/API
+        [HttpPost]
+        [Route("api/API/post_studenttable")]
+        public IHttpActionResult Post([FromBody] student_table objcls)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            objdb.student_table.Add(objcls);
+            objdb.SaveChanges();
+            return Ok(200);
+        }
+
+        // PUT: api/API/5
+        [HttpPut]
+        [Route("api/API/put_studenttable/{id}")]
+        public IHttpActionResult Put(int id, [FromBody]student_table objCls)
+        {
+            objdb.Entry(objCls).State = EntityState.Modified;
+            objdb.SaveChanges();
+            return Ok(200);
+        }
+
+        // DELETE: api/API/5
+        [HttpDelete]
+        [Route("api/API/delete_studenttable/{id}")]
+        public IHttpActionResult Delete(int id)
+        {
+            student_table student = objdb.student_table.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            objdb.student_table.Remove(student);
+            objdb.SaveChanges();
+            return Ok(student);
+        }
+    }
+}
---
+++ WebAPI/Controllers/MVCController.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAPI;
+using System.Net.Http;
+
+namespace WebAPI.Controllers
+{
+    public class MVCController : Controller
+    {
+        private WebAPIEntities db = new WebAPIEntities();
+
+        // GET: MVC
+        public ActionResult Index()
+        {
+            return View(db.student_table.ToList());
+        }
+
+        // GET: MVC/Details/5
+        public ActionResult Details(int? id)
+        {
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+            //student_table student_table = db.student_table.Find(id);
+            //if (student_table == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            else
+            {
+                student_table student = null;
+                using(var client =new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:63809/api/API/");
+                    var responseTask = client.GetAsync($"getid_studenttable/{id}");
+                    responseTask.Wait();
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<student_table>();
+                        readTask.Wait();
+                        student = readTask.Result;
+                    }
+                    else
+                    {
+                        student = new student_table();
+                    }                    
+                }
+                return View(student);
+            }            
+        }
+
+        // GET: MVC/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MVC/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Age,Mark")] student_table student_table)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.student_table.Add(student_table);
+                //db.SaveChanges();
+                using(var client=new HttpClient())//'using' is used to create the instance of the class HttpClient for the particular operation.after the execution of the code the memory optained for the object will deallocated. 
+                {
+                    client.BaseAddress = new Uri("http://localhost:63809/api/API/");//API LOCALHOST URL
+                    var postTast = client.PostAsJsonAsync<student_table>("post_studenttable", student_table);
+                    postTast.Wait();
+                    var result = postTast.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+                return View(student_table);
+            }
+            return View(student_table);
+        }
+
+        // GET: MVC/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+            //student_table student_table = db.student_table.Find(id);
+            //if (student_table == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            //return View(student_table);
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            else
+            {
+                student_table student = null;
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:63809/api/API/");
+                    var responseTask = client.GetAsync($"getid_studenttable/{id}");
+                    responseTask.Wait();
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<student_table>();
+                        readTask.Wait();
+                        student = readTask.Result;
+                    }
+                    else
+                    {
+                        student = new student_table();
+                    }
+                }
+                return View(student);
+
+
+            }
+        }
+
+        // POST: MVC/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Age,Mark")] student_table student_table)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:63809/api/API/");
+                    var postTask = client.PutAsJsonAsync<student_table>($"put_studenttable/{student_table.Id}", student_table);
+                    postTask.Wait();
+                    var result = postTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: MVC/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //student_table student_table = db.student_table.Find(id);
+            //if (student_table == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            //return View(student_table);
+
+            student_table student = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:63809/api/API/");
+                var responseTask = client.GetAsync($"getid_studenttable/{id}");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<student_table>();
+                    readTask.Wait();
+                    student = readTask.Result;
+                }
+                else
+                {
+                    student = new student_table();
+                }
+            }
+            return View(student);
+        }
+
+        // POST: MVC/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //student_table student_table = db.student_table.Find(id);
+            //db.student_table.Remove(student_table);
+            //db.SaveChanges();
+
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:63809/api/API/");
+                var postTask = client.DeleteAsync($"delete_studenttable/{id}");
+                postTask.Wait();
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Delete");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
